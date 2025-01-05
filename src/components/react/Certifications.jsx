c8714fd13eaf91d4cc57f7d92e1eb7be9634175e
Andy Chapman <achapm87@gmail.com>
2025-01-05 01:14:52 -0500

    Add certifications component

---
+++ src/components/react/Certifications.jsx
@@ -0,0 +1,42 @@
+import { Image } from 'astro:assets';
+
+const Certifications = ({ cert_data }) => {
+  return (
+    <>
+      {(cert_data && cert_data.length > 0) && (
+        <div className="grid grid-cols-2 gap-6">
+          {cert_data.map((cert, i) => {
+            const { name, issue_date, logo, url } = cert;
+            return (
+              <div key={i} className="card bg-base-200 dark:bg-neutral-content dark:text-neutral border-2 border-base-300 shadow-lg hover:shadow-xl transition-all hover:scale-105">
+                {logo && (
+                  <figure className="px-6 pt-6 h-36">
+                    {/* <Image src={logo} alt={name} /> */}
+                    <img src={logo} alt={name} className="max-w-40" loading="lazy" />
+                  </figure>
+                )}
+                <div className="card-body items-center text-center p-6">
+                  {name && (<h2 className="card-title text-lg">{name}</h2>)}
+                  {issue_date  && (
+                    <div class="issue-date">
+                      <span className="text-sm font-bold">Issued: </span>
+                      <span>{issue_date}</span>
+                    </div>
+                  )}
+                  {url && (
+                    <div className="card-actions">
+                      <a href={url} className="link link-secondary">View Cert</a>
+                    </div>
+                  )}
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Certifications;
+
