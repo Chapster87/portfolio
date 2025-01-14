0798241775c3c37e5e35dc36bbb292834bb86549
Andy Chapman <achapm87@gmail.com>
2025-01-13 19:38:07 -0500

    Additional fixes

--- src/components/react/ProjectTile.jsx
+++ src/components/react/ProjectTile.jsx
@@ -39,11 +39,11 @@ const ProjectTile = ({ content }) => {
         {platform && (
           <div className="badge badge-md badge-primary text-white mt-1">{platform}</div>
         )}
-        {short_desc && (<div className="py-3"><p className="text-md">{short_desc}</p></div>)}
+        {short_desc && (<div className="py-3"><p className="text-base">{short_desc}</p></div>)}
         <div className="card-actions justify-center mt-auto">
           {hasPage && (
             <>
-              <a href={`/project/${content.id}`} className="btn btn-secondary btn-wide" data-astro-prefetch>Read More</a>
+              <a href={`/project/${content.id}`} className="btn btn-secondary sm:btn-wide w-full" data-astro-prefetch>Read More</a>
               {url && (<a href={url} className="link" data-astro-prefetch>View Site</a>)}
             </>
           )}
